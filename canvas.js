53223374bf3be0a350eae738666512a057e5d17e
sonalparab <sparab@stuy.edu>
2018-02-03 22:16:55 -0500

    Finish

---
+++ canvas.js
@@ -0,0 +1,38 @@
+var canvas = document.getElementById("slate")
+var ctx = canvas.getContext('2d');
+var clear = document.getElementById("clear");
+var toggle = document.getElementById("toggle");
+var t = 0;
+
+var draw = function(e){
+    var offset = canvas.getBoundingClientRect();
+    var x = e.clientX - offset.x;
+    var y = e.clientY - offset.y;
+    if(t === 0){
+	ctx.beginPath();
+	ctx.arc(x,y,10,0,2 * Math.PI);
+	ctx.stroke();
+	ctx.fillStyle = "red";
+	ctx.fill();
+    }
+    else{
+	ctx.beginPath();
+	ctx.fillStyle = "blue";
+	ctx.fillRect(x-25,y-25,50,50);
+	ctx.stroke();
+    }
+}
+
+var clearing = function(e){
+    ctx.clearRect(0, 0, 500, 500);
+}
+
+var update = function(e){
+    t += 1;
+    t = t % 2;
+}
+
+canvas.addEventListener('click',draw);
+clear.addEventListener('click',clearing);
+toggle.addEventListener('click',update);
+
